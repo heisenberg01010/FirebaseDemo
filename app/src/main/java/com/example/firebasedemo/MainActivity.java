631b6c324195978be2520153ddd18d80504ef31d
JAI LODHA <54992414+heisenberg01010@users.noreply.github.com>
2020-11-28 16:24:55 +0530

    FirebaseUI added

--- app/src/main/java/com/example/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -1,166 +1,276 @@
 package com.example.firebasedemo;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.example.firebasedemo.databinding.ActivityMainBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.Source;
+import com.google.firebase.firestore.Transaction;
+import com.google.firebase.firestore.WriteBatch;
+import com.google.firebase.firestore.core.OrderBy;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class FirebaseStore extends AppCompatActivity {
 
+    private final int RC_SIGN_IN = 123;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    ActivityMainBinding b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setAccountData();
+        setDataOnFireStore();
+        deleteDataFromFstore();
+        batchWrite();
+        txn();
+        orderAccountsByBal();
+        listenToRealtimeUpdates();
 
-        b = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(b.getRoot());
+        createSignInIntent();
+        signOut();
+        delete();
 
-        setupAddFirebase();
-        getData();
     }
 
-    private void getData() {
-        CollectionReference cities = db.collection("cities");
+    public void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.TwitterBuilder().build());
 
-        Map<String, Object> data1 = new HashMap<>();
-        data1.put("name", "San Francisco");
-        data1.put("state", "CA");
-        data1.put("country", "USA");
-        data1.put("capital", false);
-        data1.put("population", 860000);
-        data1.put("regions", Arrays.asList("west_coast", "norcal"));
-        cities.document("SF").set(data1);
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+        // [END auth_fui_create_intent]
+    }
 
-        Map<String, Object> data2 = new HashMap<>();
-        data2.put("name", "Los Angeles");
-        data2.put("state", "CA");
-        data2.put("country", "USA");
-        data2.put("capital", false);
-        data2.put("population", 3900000);
-        data2.put("regions", Arrays.asList("west_coast", "socal"));
-        cities.document("LA").set(data2);
+    // [START auth_fui_result]
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        Map<String, Object> data3 = new HashMap<>();
-        data3.put("name", "Washington D.C.");
-        data3.put("state", null);
-        data3.put("country", "USA");
-        data3.put("capital", true);
-        data3.put("population", 680000);
-        data3.put("regions", Arrays.asList("east_coast"));
-        cities.document("DC").set(data3);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
 
-        Map<String, Object> data4 = new HashMap<>();
-        data4.put("name", "Tokyo");
-        data4.put("state", null);
-        data4.put("country", "Japan");
-        data4.put("capital", true);
-        data4.put("population", 9000000);
-        data4.put("regions", Arrays.asList("kanto", "honshu"));
-        cities.document("TOK").set(data4);
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+    // [END auth_fui_result]
 
-        Map<String, Object> data5 = new HashMap<>();
-        data5.put("name", "Beijing");
-        data5.put("state", null);
-        data5.put("country", "China");
-        data5.put("capital", true);
-        data5.put("population", 21500000);
-        data5.put("regions", Arrays.asList("jingjinji", "hebei"));
-        cities.document("BJ").set(data5);
-
-        db.collection("cities")
-                .whereEqualTo("name","Beijing" )
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Toast.makeText(MainActivity.this, document.getId() + " => " + document.getData(), Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Toast.makeText(MainActivity.this, "Error" + task.getException(), Toast.LENGTH_SHORT).show();
-                        }
+    public void signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
                     }
                 });
+        // [END auth_fui_signout]
+    }
 
-/*        DocumentReference docRef = db.collection("cities").document("SF");
-        //Source
-        Source source = Source.CACHE;
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-
-                    if (document.exists()) {
-                        Toast.makeText(MainActivity.this, "Document exists!", Toast.LENGTH_SHORT).show();
-                        Toast.makeText(MainActivity.this, "Cached Document Data: " + document.getData(), Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "No such document", Toast.LENGTH_SHORT).show();
+    public void delete() {
+        // [START auth_fui_delete]
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
                     }
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                });
+        // [END auth_fui_delete]
+    }
+
+
+
+    private void listenToRealtimeUpdates() {
+        db.collection("users")
+                .document("swapnil")
+                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot snapshot, @Nullable FirebaseFirestoreException e) {
+                        if (e!=null){
+                            Toast.makeText(FirebaseStore.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                        if (snapshot!=null&&snapshot.exists()){
+                            Toast.makeText(FirebaseStore.this, ""+snapshot.getId()+" "+snapshot.get("name"), Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(FirebaseStore.this, "Details does not exist!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+        ;
+    }
+
+    private void orderAccountsByBal() {
+        Query query = db.collection("accounts").orderBy("bal", Query.Direction.DESCENDING);
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot snapshot : task.getResult()) {
+                        Toast.makeText(FirebaseStore.this, "" + snapshot.getData(), Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
-        });*/
+        });
+    }
+
+    private void txn() {
+        final int amt = 20;
+        //To run a transaction
+        db.runTransaction(new Transaction.Function<Integer>() {
+            @Nullable
+            @Override
+            public Integer apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
+                //Get account a
+                Account a = transaction.get(db.collection("accounts").document("a"))
+                        .toObject(Account.class);
+                if (a.bal < amt) {
+                    return -1;
+                }
+                //Update account a by decrementing balance
+                transaction.update(db.collection("accounts").document("a"), "bal", FieldValue.increment(-amt));
+                //Update account b by increamenting balance
+                transaction.update(db.collection("accounts").document("b"), "bal", FieldValue.increment(amt));
+                return a.bal - amt;
+            }
+        }).addOnSuccessListener(new OnSuccessListener<Integer>() {
+            @Override
+            public void onSuccess(Integer bal) {
+                if (bal == -1) {
+                    new AlertDialog.Builder(FirebaseStore.this)
+                            .setTitle("Insufficient balance")
+                            .setMessage("Insufficient balance in the account")
+                            .show();
+                } else {
+                    new AlertDialog.Builder(FirebaseStore.this)
+                            .setMessage("Your balance is " + bal)
+                            .setTitle("Transaction completed")
+                            .show();
+
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(FirebaseStore.this, "Failure!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setAccountData() {
+        WriteBatch batch = db.batch();
+
+        batch.set(db.collection("accounts").document("a"), new Account(100, "a"));
+        batch.set(db.collection("accounts").document("b"), new Account(50, "b"));
+
+        batch.commit();
+    }
+
+    private void batchWrite() {
+        WriteBatch batch = db.batch();
+        DocumentReference user1 = db.collection("users")
+                .document("emiway");
+        DocumentReference user2 = db.collection("users")
+                .document("krishna");
+        DocumentReference user3 = db.collection("users")
+                .document("raftaar");
+
+        batch.update(user1, "college", "oldschool");
+        batch.update(user2, "college", "newschool");
+        batch.update(user3, "name", "raa");
+
+
+        batch.commit().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(FirebaseStore.this, "Batch writed succesfully!!", Toast.LENGTH_SHORT).show();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(FirebaseStore.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        })
+        ;
 
 
     }
 
-    private void setupAddFirebase() {
+    private void deleteDataFromFstore() {
+        db.collection("users")
+                .document("jai").delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(FirebaseStore.this, "User deleted from db", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(FirebaseStore.this, "Something went wrong!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
-        Users user1 = new Users("Ada", "Lovelace", 1816);
-        Users user2 = new Users("Isaac", "Newton", 1923 );
+    private void setDataOnFireStore() {
+        HashMap<String, Object> user = new HashMap<>();
+        user.put("name", "raftaar");
+        user.put("email", "raftaar.raa@gmail.com");
+        user.put("college", "india");
+        user.put("locality", "delhi");
 
-// Add a new document with a generated ID
-        Map<String,Object> updates = new HashMap<>();
-        updates.put("timestamp", FieldValue.serverTimestamp());
-
-        db.collection("Geniuses").document("Royal Society").collection("Sir Isaac Newton")
-                .add(user2);
-        db.collection("Geniuses").document("Mathematicians").collection("Ada Lovelace").add(user1);
-
-        DocumentReference royal_Society = db.document("Geniuses/Royal Society");
-        royal_Society.update("bornYear", 1643);
-/*
-
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Toast.makeText(MainActivity.this, "Done!", Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-*/
+        db.collection("users")
+                .document("raftaar")
+                .set(user);
 
     }
 
